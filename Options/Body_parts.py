fcb0810b8b8e91a6b634b014ac680e4526c3d008
FarahFatimaa <farahfatima787@gmail.com>
2021-05-29 22:14:54 +0500

    levels

---
+++ Options/Body_parts.py
@@ -0,0 +1,156 @@
+from tkinter import *
+from random import *
+from tkinter import messagebox
+import time
+
+#make a list of jumbled words.
+BODY_PARTS_WORD = ['HEDA', 'IHAR', 'EYE', 'SEAR', 'SEON', 'UHMTO', 'NICH', 'OEEAFHDR', 'AJW', 'EKEHC', 'BOEWEYR',
+                   'DHRSULOE', 'RSAM', 'ANDH', 'OBWEL', 'NGEIFR', 'GLE', 'FOTO', 'IHGTH', 'NEKE', ]
+
+#make a list of correct words.
+BODY_PARTS_ANSWER = ['HEAD', 'HAIR', 'EYE', 'EARS', 'NOSE', 'MOUTH', 'CHIN', 'FOREHEAD', 'JAW', 'CHEEK', 'EYEBROW',
+                     'SHOULDER', 'ARMS', 'HAND', 'ELBOW', 'FINGER', 'LEG', 'FOOT', 'THIGH', 'KNEE', ]
+
+
+ran_num = randrange(0, (len(BODY_PARTS_WORD))) #returns randomly selected element
+jumbled_rand_word = BODY_PARTS_WORD[ran_num]
+
+points = 0  #initialise point
+
+#define main funtion.
+def main():
+#this function takes user back to main window.    
+    def back():
+        my_window.destroy()
+        import index
+        index.start_main_page()
+
+#this function change the word randomly.
+    def change():
+        global ran_num
+        ran_num = randrange(0, (len(BODY_PARTS_WORD)))
+        word.configure(text=BODY_PARTS_WORD[ran_num])
+        get_input.delete(0, END)
+        ans_lab.configure(text="")
+
+#this function check answer and give score.
+    def check():
+        global points, ran_num
+        user_word = get_input.get().upper()
+        if user_word == BODY_PARTS_ANSWER[ran_num]:
+            points += 5
+            score.configure(text="Score: " + str(points))
+            messagebox.showinfo('correct', "Correct Answer.. Keep it Up!")
+            ran_num = randrange(0, (len(BODY_PARTS_WORD)))
+            word.configure(text=BODY_PARTS_WORD[ran_num])
+            get_input.delete(0, END)
+            ans_lab.configure(text="")
+        else:
+            messagebox.showerror("Error", "Inorrect Answer..Try your best!")
+            get_input.delete(0, END)
+
+#this function give hint to user if score is equal to  or greater than 5.
+    def show_answer():
+        global points
+        if points > 4:
+            points -= 5
+            score.configure(text="Score: " + str(points))
+            time.sleep(0.5)
+            ans_lab.configure(text=BODY_PARTS_ANSWER[ran_num])
+        else:
+            ans_lab.configure(text='Not enough points')
+
+#open window
+    my_window = Tk()
+    my_window.geometry("500x500")
+    my_window.resizable(0, 0)
+    my_window.title("Find the Word!")
+    my_window.configure(background="#20B2AA")
+    img1 = PhotoImage(file="back.png")
+
+#create the Button widget.
+    lab_img1 = Button(
+        my_window,
+        image=img1,
+        bg='#20B2AA',
+        border=0,
+        justify='center',
+        command=back,
+    )
+    lab_img1.pack(anchor='nw', pady=10,padx=10)
+
+#create Label widget for Score.
+    score = Label(
+        text="Score:- 0",
+        pady=10,
+        bg="#20B2AA",
+        fg="#000000",
+        font="Titillium  14 bold"
+    )
+    score.pack(anchor="n")    
+
+#create Label widget.
+    word = Label(
+        text=jumbled_rand_word,
+        pady=10,
+        bg="#20B2AA",
+        fg="#000000",
+        font="Titillium  50 bold"
+    )
+    word.pack()
+
+#create Entry widget.
+    get_input = Entry(
+        font="none 26 bold",
+        borderwidth=10,
+        justify='center',
+    )
+    get_input.pack()        
+
+#create a Button widget.The text Submit should appear on the face of the Button.    
+    submit = Button(
+        text="Submit",
+        width=18,
+        borderwidth=8,
+        font=("", 13),
+        fg="#000000",
+        bg="#f08080",
+        command=check,   
+    )
+    submit.pack(pady=(10,20))
+
+#create a Button widget.The text Change the word should appear on the face of the Button.
+    change = Button(
+        text="Change Word",
+        width=18,
+        borderwidth=8,
+        fg="#000000",
+        bg="#f08080",
+        font=("", 13),
+        command=change,     
+    )
+    change.pack()
+
+#create a Button widget.The text Answer should appear on the face of the Button.
+    ans = Button(
+        text="Answer",
+        width=18,
+        borderwidth=8,
+        fg="#000000",
+        bg="#f08080",
+        font=("", 13),
+        command=show_answer,    
+    )
+    ans.pack(pady=(20, 10))
+
+#create Label widget.
+    ans_lab = Label(
+        text="",
+        bg="#20B2AA",
+        fg="#000000",
+        font="Courier 15 bold",
+    )
+    ans_lab.pack()
+
+#enter the main loop.
+    my_window.mainloop()
---
+++ Options/Instructions.py
@@ -0,0 +1,114 @@
+from tkinter import *
+
+#this funtion writes four lines of data to a file.
+#define the file function.
+def file():
+    
+#open a file named how to play.txt.    
+    infile = open('Steps.txt' , 'w')
+    
+#wrie the steps to the file.
+    infile.write("Step 1:Click on start button.\n")
+    infile.write("Step 2:Select any level you want to play.\n")
+    infile.write("Step 3:Write correct answer in label.\n")
+    infile.write("Step 4:Then press submit button.\n")
+    infile.write("Now you are ready.Click arrow and start the game.\n")
+    infile.write("Best of luck!\n")
+
+#close the file.    
+    infile.close()
+
+#call the file function.    
+file()
+
+#define main function.
+def main():    
+    def back():
+        my_window.destroy()
+        import index
+        index.start_main_page()
+
+    def all_steps():
+        try:
+            #open the file.
+            infile=open('Steps.txt', 'r')
+
+            #read the file contents.
+            contents= infile.read()
+
+            #close the file.
+            infile.close()
+
+            #create a Label widget.
+            lab1=Label(
+                my_window,
+                text=contents,
+                bg='#20B2AA',
+                borderwidth=8,
+                font=("Calibri", 15 )
+            )
+            lab1.pack(pady=(50, 0))
+            
+        except IOError:
+            error= "File not found"
+
+            #create a Label widget.
+            lab2=Label(
+                my_window,
+                text=error,
+                 bg='#20B2AA',
+                width=18,
+                borderwidth=8,
+                font=("Calibri", 19)
+            )
+            lab2.pack(pady=(50, 0))
+
+        
+#open window
+    my_window = Tk()
+    my_window.geometry("500x500")
+    my_window.resizable(0, 0)
+    my_window.title("Find The Word Game!")
+    my_window.configure(background="#20B2AA")
+
+#inser image.    
+    img1 = PhotoImage(file="back.png")
+
+#create the Button widget.
+    lab_img1 = Button(
+        my_window,
+        image=img1,
+        bg=("#20B2AA"),
+        border=0,
+        justify='center',
+        command=back,         #function call
+    )
+    
+    lab_img1.pack(anchor='nw', pady=10, padx=10)
+
+#create Label widget with text Instructions.
+    lab = Label(
+        text="Instructions",
+        pady=10,
+        bg='#20B2AA',
+        fg="#000000",
+        font=("Times New Roman", 30)
+    )
+    lab.pack(anchor="n")
+
+#create Button widget with the text Follow these steps.
+    steps = Button(
+        text="Follow these steps",
+        width=18,
+        borderwidth=8,
+        fg="#000000",
+        bg="#f08080",
+        font=("", 13),
+        command=all_steps,   #function called
+    )
+    steps.pack(pady=(20, 10))
+    
+#enter the main loop.
+    my_window.mainloop()
+
+    
